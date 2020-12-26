423211fd26a01d4af5fa816ba1385511b26219c1
Angrey Shoful <shoful.a@gmail.com>
2020-12-26 10:45:53 +0200

    **

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
+const createCell = (num) => 
+  Array(num**2).fill('').map((_, i) => {
+    let row = Math.floor(i / num);
+    let cell =  i % num;
+    let color = (row + cell) % 2 === 0 
+    ? 'white' : 'black';
+    let target = String(row) + String(cell)
+
+    return {color, target} 
+  })
+
+const shift = (x, y) => [-10 * x - y, -10 * x + y, 10 * x - y, 10 * x + y, 
+          -10 * y - x, -10 * y + x, 10 * y - x, 10 * y + x]
+
+const stepHorse = (num) => shift(2, 1).map(index => index + num)
+
 function App() {
+
+  const [green, setGreen] = React.useState('');
+  const [horse, setHorse] = React.useState([])
+  const fieldWidth = 320;
+  const cell = 8;
+
+  const handleClick = e => {
+    let target = e.target.getAttribute('target');
+    setGreen(target)
+    setHorse(stepHorse(+target))
+  }
+
+  const context = createCell(cell).map( (index, i) => 
+    <div key={i} 
+      {...index} 
+      style={{width: fieldWidth/cell, height: fieldWidth/cell }}
+      className={index.target === green ? 
+        "cell green " + index.color :
+        "cell " + index.color  }>
+        {horse.includes(+index.target) ? 'h' : ''}
+    </div>) 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+      <div className="App-header">
+       <h1>Chess</h1>
+       <div className="field" onClick={handleClick}>
+        {context} 
+       </div>
+       </div>
+    </main>
   );
 }
 
