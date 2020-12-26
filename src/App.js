3237513868126d67de60c0ea3313b587e3b77799
Angrey Shoful <shoful.a@gmail.com>
2020-12-26 16:02:44 +0200

    added input

--- src/App.js
+++ src/App.js
@@ -22,38 +22,55 @@ const stepsKnight = (num) => shift(2, 1).map(index =>
 function App() {
 
   const [green, setGreen] = React.useState([]);
-  const [knight, setKnight] = React.useState([])
+  const [input, setInput] = React.useState('');
+  const [size, setSize] = React.useState(8);
+  const [knight, setKnight] = React.useState([]);
   const fieldWidth = 320;
-  const cell = 8;
-
+  
   const handleClick = e => {
     let target = e.target.getAttribute('target').split(',').map(i => +i);
     setGreen(target)
     setKnight(stepsKnight(target))
   }
 
-  const checkTarget = (arr, target) => 
+  const checkMoveOfTheKnight = (arr, target) => 
     arr.map(index => 
       index[0] === target[0] && index[1] === target[1])
     .reduce((res, i) => res + i, 0)
   
+  
+  const hangleSubmit = (e) => {
+    e.preventDefault()
+    if (Number.isInteger(+input) && +input > 0 && +input <= 25) {
+      setSize(input)
+   }
+  }
 
-  const context = createCell(cell).map( (index, i) => 
+  const context = createCell(size).map( (index, i) => 
     <div key={i} 
       {...index} 
-      style={{width: fieldWidth/cell, 
-        height: fieldWidth/cell, 
-        fontSize: fieldWidth/cell/1.5 }}
+      style={{width: fieldWidth/size, 
+        height: fieldWidth/size, 
+        fontSize: fieldWidth/size/1.5 }}
       className={(index.target[0] === green[0] && index.target[1] === green[1]) ? 
         "cell green " + index.color :
         "cell " + index.color  }>
-        {checkTarget(knight, index.target) ? 'h' : ''}
+        {checkMoveOfTheKnight(knight, index.target) ? 'h' : ''}
     </div>) 
 
   return (
     <main className="App">
-      <div className="App-header">
+      <div className="App-main">
        <h1>Move of the knight</h1>
+       <form onSubmit={hangleSubmit}>
+        <input
+          className="Input"
+          type='text'
+          value={input}
+          placeholder="select a board size from 1 to 25"
+          onChange={(e) => setInput(e.target.value)}
+          />
+        </form>
        <div className="field" onClick={handleClick}>
         {context} 
        </div>
