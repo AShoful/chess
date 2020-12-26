803b7c942745a0bcdcb9222fd2429c3b4ab8cd9d
Angrey Shoful <shoful.a@gmail.com>
2020-12-26 14:59:00 +0200

    added the ability to change the board size

--- src/App.js
+++ src/App.js
@@ -7,37 +7,47 @@ const createCell = (num) =>
     let cell =  i % num;
     let color = (row + cell) % 2 === 0 
     ? 'white' : 'black';
-    let target = String(row) + String(cell)
+    let target = [row, cell]
 
     return {color, target} 
   })
 
-const shift = (x, y) => [-10 * x - y, -10 * x + y, 10 * x - y, 10 * x + y, 
-          -10 * y - x, -10 * y + x, 10 * y - x, 10 * y + x]
+const shift = (x, y) => [[-1 * x, -1 * y], [-1 * x, y], [x, y],
+                        [x, -1 * y], [-1 * y, -1 * x], [y, x],
+                         [-1 * y,  x], [y, -1 * x] ]
 
-const stepKnight = (num) => shift(2, 1).map(index => index + num)
+const stepsKnight = (num) => shift(2, 1).map(index => 
+  [index[0] + num[0], index[1] + num[1]])
 
 function App() {
 
-  const [green, setGreen] = React.useState('');
+  const [green, setGreen] = React.useState([]);
   const [knight, setKnight] = React.useState([])
   const fieldWidth = 320;
   const cell = 8;
 
   const handleClick = e => {
-    let target = e.target.getAttribute('target');
+    let target = e.target.getAttribute('target').split(',').map(i => +i);
     setGreen(target)
-    setKnight(stepKnight(+target))
+    setKnight(stepsKnight(target))
   }
+
+  const checkTarget = (arr, target) => 
+    arr.map(index => 
+      index[0] === target[0] && index[1] === target[1])
+    .reduce((res, i) => res + i, 0)
+  
 
   const context = createCell(cell).map( (index, i) => 
     <div key={i} 
       {...index} 
-      style={{width: fieldWidth/cell, height: fieldWidth/cell }}
-      className={index.target === green ? 
+      style={{width: fieldWidth/cell, 
+        height: fieldWidth/cell, 
+        fontSize: fieldWidth/cell/1.5 }}
+      className={(index.target[0] === green[0] && index.target[1] === green[1]) ? 
         "cell green " + index.color :
         "cell " + index.color  }>
-        {knight.includes(+index.target) ? 'h' : ''}
+        {checkTarget(knight, index.target) ? 'h' : ''}
     </div>) 
 
   return (
