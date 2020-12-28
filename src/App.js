c6db8dd34bc07c128c8c4d95533440a15648d56c
Angrey Shoful <shoful.a@gmail.com>
2020-12-28 16:04:55 +0200

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 const createCell = (num) => 
@@ -6,25 +6,25 @@ const createCell = (num) =>
     let row = Math.floor(i / num);
     let cell =  i % num;
     let color = (row + cell) % 2 === 0 
-    ? 'white' : 'black';
+    ? 'cell bg-light' 
+    : 'cell bg-dark text-white';
     let target = [row, cell]
 
     return {color, target} 
   })
 
 const shift = (x, y) => [[-1 * x, -1 * y], [-1 * x, y], [x, y],
-                        [x, -1 * y], [-1 * y, -1 * x], [y, x],
-                         [-1 * y,  x], [y, -1 * x] ]
+  [x, -1 * y], [-1 * y, -1 * x], [y, x], [-1 * y,  x], [y, -1 * x]]
 
 const stepsKnight = (num) => shift(2, 1).map(index => 
   [index[0] + num[0], index[1] + num[1]])
 
 function App() {
 
-  const [green, setGreen] = React.useState([]);
-  const [input, setInput] = React.useState('');
-  const [size, setSize] = React.useState(8);
-  const [knight, setKnight] = React.useState([]);
+  const [green, setGreen] = useState([]);
+  const [input, setInput] = useState('');
+  const [size, setSize] = useState(8);
+  const [knight, setKnight] = useState([]);
   const fieldWidth = 320;
   
   const handleClick = e => {
@@ -34,28 +34,38 @@ function App() {
   }
 
   const checkMoveOfTheKnight = (arr, target) => 
-    arr.map(index => 
-      index[0] === target[0] && index[1] === target[1])
-    .reduce((res, i) => res + i, 0)
+    arr.map(index => String(index) === String(target))
+      .reduce((res, i) => res + i, 0)
   
   
   const hangleSubmit = (e) => {
     e.preventDefault()
     if (Number.isInteger(+input) && +input > 0 && +input <= 25) {
       setSize(input)
+      setInput('')
    }
   }
-
+  
+  const icon = <svg xmlns="http://www.w3.org/2000/svg" 
+    width="16" 
+    height="16" 
+    fill="currentColor" 
+    className="bi bi-vinyl" 
+    viewBox="0 0 16 16">
+    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+    <path d="M8 6a2 2 0 1 0 0 4 2 2 0 0 0 0-4zM4 8a4 4 0 1 1 8 0 4 4 0 0 1-8 0z"/>
+    <path d="M9 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
+  </svg>
+  
   const context = createCell(size).map( (index, i) => 
     <div key={i} 
       {...index} 
-      style={{width: fieldWidth/size, 
-        height: fieldWidth/size, 
-        fontSize: fieldWidth/size/1.5 }}
-      className={(index.target[0] === green[0] && index.target[1] === green[1]) ? 
-        "cell green " + index.color :
-        "cell " + index.color  }>
-        {checkMoveOfTheKnight(knight, index.target) ? 'h' : ''}
+      style={{width: (fieldWidth - 4)/size, 
+        height: (fieldWidth - 4)/size}}
+      className={String(green) === String(index.target) ? 
+        "cell bg-success" :
+        index.color  }>
+        {checkMoveOfTheKnight(knight, index.target) ? icon : ''}
     </div>) 
 
   return (
